99c4f95daafc1f33c4aeff0ae59925545f8e691f
may-h <hms9210@naver.com>
2020-04-21 17:52:51 +0900

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Button danger>Hello</Button>
+        <Button>Hello</Button>
+      </Container>
+    );
+  }
 }
 
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: lightgrey;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${(props) => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+//ES6 화살표 문법을 사용한 함수형 컴포넌트
+// const Button = ({ danger }) => (
+//   <button className={danger ? "button--danger" : "button--success"}>
+//     Hello
+//   </button>
+// );
+
+//함수형 컴포넌트
+// function Button({ danger }) {
+//   return (
+//     <button className={danger ? "button--danger" : "button--success"}>
+//       Hello
+//     </button>
+//   );
+// }
 export default App;
